49e04930b324cfbc740131438516ce476707553e
nayakan <nayakj9@gmail.com>
2017-04-06 12:19:13 -0700

    Update Multiplexor.v

--- Multiplexor.v
+++ Multiplexor.v
@@ -1,3 +1,19 @@
+// Multiplexor for calculating branch address
+module MIPSMUX0(offset, PCplus1,AluZeroOP,branch,Mux0Out); 
+	input[31:0]offset,PCplus1;
+	input AluZeroOP, branch;
+	wire Mux0Sel;
+	output reg[31:0]Mux0Out;
+	assign Mux0Sel = branch & AluZeroOP; 
+	always@(Mux0Sel, offset, PCplus1) 
+	begin
+		case(Mux0Sel)
+			0:Mux0Out <= PCplus1; 
+			1:Mux0Out = offset + PCplus1;
+		endcase 
+	end 
+endmodule
+
 // Multiplexor to select destination register in Register Module
 module MIPSMUX1(Mux1In0,Mux1In1,Mux1Sel,Mux1Out); 
 	input[4:0]Mux1In0,Mux1In1;
@@ -36,22 +52,6 @@ module MIPSMUX3(Mux3In0,Mux3In1,Mux3Sel,Mux3Out, RV);
 					RV <= Mux3In0; end
 			1:begin Mux3Out <= Mux3In1;
 					RV <= Mux3In1; end
-		endcase 
-	end 
-endmodule
-
-// Multiplexor for calculating branch address
-module MIPSMUX0(offset, PCplus1,AluZeroOP,branch,Mux0Out); 
-	input[31:0]offset,PCplus1;
-	input AluZeroOP, branch;
-	wire Mux0Sel;
-	output reg[31:0]Mux0Out;
-	assign Mux0Sel = branch & AluZeroOP; 
-	always@(Mux0Sel, offset, PCplus1) 
-	begin
-		case(Mux0Sel)
-			0:Mux0Out <= PCplus1; 
-			1:Mux0Out = offset + PCplus1;
 		endcase 
 	end 
 endmodule
