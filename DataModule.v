c7bbec6d8d14cdfe1b6ca6b682587bdccc454c56
nayakan <nayakj9@gmail.com>
2017-04-06 11:52:33 -0700

    Update DataModule.v

--- DataModule.v
+++ DataModule.v
@@ -5,13 +5,18 @@ module MIPSDM(Write_data,Read_data,Mem_write, Mem_read,DMaddress,clock, SWdata);
 	reg[31:0]Read_data; 
 	reg[31:0]MIPSDM[511:0]; 
 	output reg[31:0] SWdata;
-	initial begin MIPSDM[5] <= 7; 
-end
+	initial begin 
+		MIPSDM[5] <= 7; 
+	end
 
-always @( negedge clock)
-begin
-	if(Mem_read && !Mem_write) Read_data<=MIPSDM[DMaddress]; end
-always @(posedge clock)
-begin
-	if(Mem_write && !Mem_read ) MIPSDM[DMaddress]<=Write_data; SWdata <= MIPSDM[DMaddress]; end
+	always @( negedge clock)
+	begin
+		if(Mem_read && !Mem_write) Read_data<=MIPSDM[DMaddress]; 
+	end
+	
+	always @(posedge clock)
+	begin
+		if(Mem_write && !Mem_read ) MIPSDM[DMaddress]<=Write_data; SWdata <= MIPSDM[DMaddress]; 
+	end
+	
 endmodule
